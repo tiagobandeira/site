c57f126df36926309264417a885a195d8adca5c7
Tiago Bandeira <tiagobandeirabarros@gmail.com>
2017-08-26 11:50:58 -0300

    Editando página inicial

--- index.php
+++ index.php
@@ -201,7 +201,7 @@
                     <div style="padding: 10px;">
                         <img width='70%' height="125px" src="midia/<?php echo $row['imagem']; ?>">
                     </div>
-                     <?php echo $row['texto']; ?><span class="badge">8</span>
+                     <p><?php echo $row['texto']; ?><span class="badge">8</span></p>
                   </div>
               </div>
               <?php  
