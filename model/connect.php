3d95c405a4424c35e9799c57b31006a0f0775756
Tiago Bandeira <tiagobandeirabarros@gmail.com>
2017-08-25 23:45:03 -0300

    Configurando conexão com Banco de dados

--- model/connect.php
+++ model/connect.php
@@ -1,7 +1,7 @@
 <?php  
 	$user = "u599766642_tiago";
 	$pass = "bS9EgRmy8q1r";
-	$host = "localhost";
+	$host = "127.0.0.1";
 	$bd = "u599766642_site";
 
 	$con = new mysqli($host, $user, $pass, $bd);
